7f9c76a8ad3094fdd135e25889f018f6778b2a11
alexcostabd <alexcosta1624@gmail.com>
2020-07-05 17:50:44 -0300

    #VIEW E SELECT ADICIONADOS

--- PROJETO_FINAL_SELECT.sql
+++ PROJETO_FINAL_SELECT.sql
@@ -1,9 +1,9 @@
 /*
-* VERIFICA OS RECEBIMENTOS DE USUÃRIOS EM UMA DETERMINADA FAIXA DE TEMPO.
+* VERIFICA OS RECEBIMENTOS DE USUÃ?RIOS EM UMA DETERMINADA FAIXA DE TEMPO.
 */
 SELECT 
-    USU.usu_cpf "CPF DO USUÃRIO", 
-    USU.usu_nome "NOME DO USUÃRIO",
+    USU.usu_cpf "CPF DO USUÃ?RIO", 
+    USU.usu_nome "NOME DO USUÃ?RIO",
     TRA.traEmp_id "NÃšMERO DA TRANSAÃ‡ÃƒO",
     TRA.traEmp_valorLiquido "VALOR LIQUIDO PAGO"
 
@@ -16,15 +16,15 @@ WHERE
 
 
 /*
-* VERIFICA OS RECEBIMENTO DE USUÃRIOS EM UMA DETERMINADA FAIXA DE TEMPO.
+* VERIFICA OS RECEBIMENTO DE USUÃ?RIOS EM UMA DETERMINADA FAIXA DE TEMPO.
 */
 SELECT
-    DISTINCT USU.usu_nome "NOME FUNCIONÃRIO",
+    DISTINCT USU.usu_nome "NOME FUNCIONÃ?RIO",
     TEL.tel_ddd "DDD",
     TEL.tel_numero "TELEFONE",
     STR.tip_descricao "SETOR",
     EMP.emp_nome_fantasia "NOME DA EMPRESA",
-    TPF.funcao_salario "SALÃRIO"
+    TPF.funcao_salario "SALÃ?RIO"
 FROM 
     USUARIO USU, 
     FUNCIONARIO FUN,
@@ -43,6 +43,28 @@ WHERE
     UPPER(STR.tip_descricao) = 'PEDAGOGICO' AND
     TPF.funcao_salario BETWEEN '2000.00' AND '4000.00'
     ORDER BY USU.usu_nome;
+    
+    
+SELECT 
+    USU.usu_nome "NOME DO USUÁRIO", 
+    USU.usu_cpf "CPF DO USUÁRIO",
+    TRA.trans_data "DATA PARA PAGAMENTO",
+    TREMP.traemp_datatransacao "DATA DE PAGAMENTO",
+    EMP.emp_nome_fantasia "NOME DA EMPRESA",
+    TREMP.traEmp_descricao "DESCRIÇÃO DA TRANSAÇÃO",
+    'R$ ' || TREMP.traEmp_valorLiquido "VALOR LIQUIDO PAGO"
+FROM 
+    USUARIO USU, 
+    TRANSACAO TRA,
+    TRANSACAO_EMPRESA TREMP,
+    EMPRESA EMP
+WHERE
+    USU.usu_id = TREMP.usu_id AND 
+    TREMP.trans_id = TRA.trans_id AND
+    TREMP.traemp_datatransacao > TRA.trans_data AND
+    UPPER(TRA.TRANS_DESCRICAO) LIKE 'R%' AND
+    TREMP.traemp_datatransacao BETWEEN 
+    TO_DATE(nvl('','01/06/2020'),'DD/MM/YYYY') AND TO_DATE(nvl('','30/06/2020'),'DD/MM/YYYY');
 
 
 
